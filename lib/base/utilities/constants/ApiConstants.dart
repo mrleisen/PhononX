fa6d853001c6e54edcce7b00f4efdce0757c1ecd
mrleisen <ing.rafaelcamargo@gmail.com>
2022-06-21 19:58:41 -0500

    updating api constants

--- lib/base/utilities/constants/ApiConstants.dart
+++ lib/base/utilities/constants/ApiConstants.dart
@@ -4,9 +4,12 @@ class ApiConstants {
 
   static const API = "https://api.github.com/";
   static const API_GET_USERS = API + "users/";
+  static const API_SEARCH_USERS = API + "search/users?q=";
 
   // these are local errors
   static const API_GET_USERS_FAILED = "99999";
   static const GET_USERS_FAILED_IN_DATA_SOURCE = "99998";
+  static const API_SEARCH_USERS_FAILED = "99997";
+  static const SEARCH_USERS_FAILED_IN_DATA_SOURCE = "99996";
 
 }
